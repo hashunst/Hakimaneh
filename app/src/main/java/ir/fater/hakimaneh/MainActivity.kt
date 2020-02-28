d65bf404fd9c1199fa5508dc8052482e05757099
hashunst <hash.un.st@gmail.com>
2020-02-28 19:55:58 +0330

    translate TextView to persian

--- app/src/main/java/ir/fater/hakimaneh/MainActivity.kt
+++ app/src/main/java/ir/fater/hakimaneh/MainActivity.kt
@@ -2,11 +2,13 @@ package ir.fater.hakimaneh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv.text = "سلام دنیا!"
     }
 }

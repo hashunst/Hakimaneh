2ee0e2122ce59429c80540ca4169bd080cf18ce3
mohammad <mohammad673029@yahoo.com>
2020-02-28 20:12:55 +0330

    سلام تغیرات ایجاد شد نوشته شد سلام چترود

--- app/src/main/java/ir/fater/hakimaneh/MainActivity.kt
+++ app/src/main/java/ir/fater/hakimaneh/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv.text = "سلام دنیا!"
+        tv.text ="سلام چترود!"
     }
 }
